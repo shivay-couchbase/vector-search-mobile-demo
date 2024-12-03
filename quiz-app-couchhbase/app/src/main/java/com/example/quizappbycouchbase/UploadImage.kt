a70409a2b7d463eb92c82a6c90f2ee786315a5c6
belio <elio.salvatore@couchbase.com>
2024-12-03 14:44:52 +0100

    bump to final release

--- quiz-app-couchhbase/app/src/main/java/com/example/quizappbycouchbase/UploadImage.kt
+++ quiz-app-couchhbase/app/src/main/java/com/example/quizappbycouchbase/UploadImage.kt
@@ -36,7 +36,7 @@ class UploadImage : AppCompatActivity() {
 
     val retrofit = Retrofit.Builder()
         .baseUrl("http://10.0.2.2:5000")
-//        .baseUrl("http://127.0.0.1:5000")
+   //    .baseUrl("http://127.0.0.1:5000")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
