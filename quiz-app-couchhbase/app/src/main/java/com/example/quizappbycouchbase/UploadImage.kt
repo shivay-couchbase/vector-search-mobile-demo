80743675ef2e2fadab6ae854d8aa06fa867823ed
belio <elio.salvatore@couchbase.com>
2024-12-05 11:42:39 +0100

    cleaning

--- quiz-app-couchhbase/app/src/main/java/com/example/quizappbycouchbase/UploadImage.kt
+++ quiz-app-couchhbase/app/src/main/java/com/example/quizappbycouchbase/UploadImage.kt
@@ -13,11 +13,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.documentfile.provider.DocumentFile
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
